a80c394c46a53f4159ada35ef25ad4c32df263e3
Golam Rabbani <66532594+KhRabbani@users.noreply.github.com>
2025-03-15 23:54:41 +0600

    admin 1st commit

--- admin.php
+++ admin.php
@@ -19,6 +19,28 @@
         
 </fieldset>
 
+<fieldset>
+        <legend>Personal Information</legend>
+        <label for="first-name">First Name:</label>
+        <input type="text" id="first-name" name="first-name"><br><br>
+        
+        <label for="sure-name">Sure Name:</label>
+        <input type="text" id="sure-name" name="sure-name"><br><br>
+        
+        <label for="phone">Phone Number:</label>
+        <input type="tel" id="phone" name="phone"><br><br>
+        
+        <label for="dob">Date of Birth:</label>
+        <input type="date" id="dob" name="dob"><br><br>
+        
+        <label for="address">Address:</label>
+        <input type="text" id="address" name="address"><br><br>
+
+        <label for="Email">Email:</label>
+        <input type="text" id="Email" name="Email"><br><br>
+
+    </fieldset>
+
 <form action="Admin_Home.php">
         <button type="Login">Login</button>
 </form>
